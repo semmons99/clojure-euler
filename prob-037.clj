4155a70ca8807c4f4342d30b3306b11f984b3912
Shane Emmons <shane.emmons@fmins.com>
2009-07-15 07:57:42 -0400

    added problem 037

---
+++ prob-037.clj
@@ -0,0 +1,26 @@
+;;;;;;;;;; problem 037 ;;;;;;;;;;
+(use '[clojure.contrib.lazy-seqs :only (primes)]
+     '[clojure.contrib.seq-utils :only (includes?)])
+(defn prime? [n] (includes? (take-while #(<= % n) primes) n))
+
+(defn rrest [s]
+  (rest (rest s)))
+
+(defn truncate
+  ([n dir] (truncate n dir []))
+  ([n dir coll]
+     (if (zero? n)
+       coll
+       (recur
+        (if (= dir :right)
+          (quot n 10)
+          (let [a (apply str (rrest (.split (str n) "")))]
+            (if (= a "") 0 (Integer. a))))
+        dir (cons n coll)))))
+
+(defn prob-037 []
+  (reduce + (take 11 (for [n primes
+                           :when (and (> n 10)
+                                      (every? prime? (truncate n :right))
+                                      (every? prime? (truncate n :left)))]
+                       n))))
