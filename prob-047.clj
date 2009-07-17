93c73228921219150d9c06851fe9ab3aaab68026
Shane Emmons <shane.emmons@fmins.com>
2009-07-17 16:41:18 -0400

    added problem 047

---
+++ prob-047.clj
@@ -0,0 +1,21 @@
+;;;;;;;;;; problem 047 ;;;;;;;;;;
+(use '[clojure.euler.helpers :only (prime?)])
+
+(defn factors [n]
+  (reduce into (for [x (range 1 (inc (Math/sqrt n)))
+                     :when (zero? (mod n x))]
+                 [x (/ n x)])))
+
+(defn prime-factors [n]
+  (filter prime? (distinct (factors n))))
+(def prime-factors (memoize prime-factors))
+
+(defn prob-047 []
+  (loop [n 210]
+    (if (= 4
+           (count (prime-factors n))
+           (count (prime-factors (+ n 1)))
+           (count (prime-factors (+ n 2)))
+           (count (prime-factors (+ n 3))))
+      n
+      (recur (inc n)))))
