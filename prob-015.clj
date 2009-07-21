274e3683609714c592beb23e952da2d40c48bd93
Shane Emmons <shane.emmons@fmins.com>
2009-07-21 08:02:50 -0400

    fixed 'choose' function in problem 015

--- prob-015.clj
+++ prob-015.clj
@@ -4,7 +4,7 @@
   (reduce * (range 1 (inc n))))
 
 (defn choose [n r]
-  (/ (factorial n) (expt (factorial r) 2)))
+  (/ (factorial n) (* (factorial r) (factorial (- n r)))))
 
 (defn prob-015 []
   (choose (* 20 2) 20))
