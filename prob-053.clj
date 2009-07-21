594a42bdbdef707bb89a7270e81f099fdea37b93
Shane Emmons <shane.emmons@fmins.com>
2009-07-21 07:59:02 -0400

    added problem 053

---
+++ prob-053.clj
@@ -0,0 +1,14 @@
+;;;;;;;;;; problem 053 ;;;;;;;;;;
+(use '[clojure.contrib.math :only (expt)])
+(defn factorial [n]
+  (reduce * (range 1 (inc n))))
+
+(defn choose [n r]
+  (/ (factorial n) (* (factorial r) (factorial (- n r)))))
+
+(defn prob-053 []
+  (count (for [n (range 1 101)
+               r (range 1 (inc n))
+               :let [c (choose n r)]
+               :when (> c 1000000)]
+           c)))
