d14e51f21bd7eff93ebcef165945c930efc7f348
Shane Emmons <shane.emmons@fmins.com>
2009-07-27 10:47:59 -0400

    updated problem 045 to use triangles, pentagonals and hexagonals

--- prob-045.clj
+++ prob-045.clj
@@ -1,18 +1,7 @@
 ;;;;;;;;;; problem 045 ;;;;;;;;;;
-(defn nth-triangle [n] (/ (* n (inc n)) 2))
-
-(defn triangles [] (map nth-triangle (iterate inc 2)))
-
-(defn nth-pentagonal [n] (/ (* n (dec (* 3 n))) 2))
-
-(defn pentagonals [] (map nth-pentagonal (iterate inc 2)))
-
-(defn nth-hexagonal [n] (* n (dec (* 2 n))))
-
-(defn hexagonals [] (map nth-hexagonal (iterate inc 2)))
+(use '[clojure.euler.helpers :only (triangles pentagonals hexagonals)])
 
 (defn prob-045 []
-  (let [ts (apply sorted-set (take 100000 (triangles)))
-        ps (apply sorted-set (take 100000 (pentagonals)))
-        hs (apply sorted-set (take 100000 (hexagonals)))]
-    (second (filter #(and (ps %) (hs %)) ts))))
+  (let [ts (apply sorted-set (take 100000 triangles))
+        ps (apply sorted-set (take 100000 pentagonals))]
+    (second (filter #(and (ts %) (ps %)) (rest hexagonals)))))
