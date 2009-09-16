56df682a9d96be14b4044c0a1c60e69fed10dd51
Shane Emmons <shane.emmons@fmins.com>
2009-09-16 07:59:46 -0400

    modified prob-001 to use closures

--- prob-001.clj
+++ prob-001.clj
@@ -1,6 +1,7 @@
 ;;;;;;;;;; problem 001 ;;;;;;;;;;
-(defn multiple-of-3 [n] (= 0 (mod n 3)))
-(defn multiple-of-5 [n] (= 0 (mod n 5)))
+(defn multiple-of [n] #(zero? (mod % n)))
+(def multiple-of-3 (multiple-of 3))
+(def multiple-of-5 (multiple-of 5))
 
 (defn prob-001 []
   (reduce + (filter #(or (multiple-of-3 %) (multiple-of-5 %)) (range 1000))))
