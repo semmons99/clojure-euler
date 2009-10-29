3480bc313b9df7f282dadf6e0b48d96230f1bfc1
Shane Emmons <shane.emmons@fmins.com>
2009-10-29 07:48:12 -0400

    added multiple-of-3-or-5 to prob-001

--- prob-001.clj
+++ prob-001.clj
@@ -2,6 +2,7 @@
 (defn multiple-of [n] #(zero? (mod % n)))
 (def multiple-of-3 (multiple-of 3))
 (def multiple-of-5 (multiple-of 5))
+(defn multiple-of-3-or-5 [n] (or (multiple-of-3 n) (multiple-of-5 n)))
 
 (defn prob-001 []
-  (reduce + (filter #(or (multiple-of-3 %) (multiple-of-5 %)) (range 1000))))
+  (reduce + (filter multiple-of-3-or-5 (range 1000))))
